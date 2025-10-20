d35f66ff12ddb6228937eb4cc5f41b7a649b5562
G2284512022 <228451@st.takushoku-u.ac.jp>
2025-10-21 02:06:46 +0900

    空白行削除

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 df = pd.read_excel("deta.xlsx")
+df = df.dropna(axis=1, how="all")
+df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
 
 #データ数
 print("データ数:", df.shape)
@@ -11,7 +13,7 @@ print("データ数:", df.shape)
 df.to_csv("backup_raw.csv", index=False, encoding="utf-8-sig")
 
 #データ抽出(状況のまとめ)
-print(df["状況のまとめ"].astype(str).value_counts().head(10))
+print(df["状況のまとめ"].value_counts())
 
 #(目標変数カテゴリ作り)
 df["失踪の有無"] = df["状況のまとめ"].astype(str).str.contains("失踪", na=False).astype("int8")
@@ -24,9 +26,6 @@ print(df_f["状況のまとめ"].value_counts())
 
 #失踪率
 print("失踪率：", df_f["失踪の有無"].mean())
-
-#テスト
-#print(df_f[["状況のまとめ","失踪の有無"]].head(10))
 
 #目標変数
 y = df_f["失踪の有無"]
@@ -42,20 +41,8 @@ X = df_f[sf + cf].copy()
 
 # 欠損値補完
 X["入国時年齢"] = X["入国時年齢"].fillna(X["入国時年齢"].median())
-#X["失踪までの在日日数"] = X["失踪までの在日日数"].fillna(0)
 for col in cf:
     X[col] = X[col].astype("string").fillna("N/A")
-
-# 学習用とテスト用
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=42, stratify=y
-)
-
-# test
-print("学習データサイズ:", X_train.shape)
-print("テストデータサイズ:", X_test.shape)
-print("学習データ失踪率:", y_train.mean())
-print("テストデータ失踪率:", y_test.mean())
 
 # 学習用データ保存
 clean = X.copy()
